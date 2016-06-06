ee8097b392e2b7934983a6f3bd357e8c232574d1
Matthew Payne <balancetemp@gmail.com>
2016-06-06 13:08:46 -0400

    program using hashedId

--- js/loving_meditations_script.js
+++ js/loving_meditations_script.js
@@ -58,7 +58,7 @@
         $scope.selectProgram = function(programId, programHashedId) {
             if ($scope.selectedProgramId == -1) {
                 $scope.selectedProgramId = programId;
-                $scope.selectedProgramEmbedSrc = "//fast.wistia.net/embed/playlists/" + programId + "?media_0_0%5BautoPlay%5D=false&media_0_0%5BcontrolsVisibleOnLoad%5D=false&theme=tab&version=v1&videoOptions%5BautoPlay%5D=true&videoOptions%5BvideoHeight%5D=360&videoOptions%5BvideoWidth%5D=640&videoOptions%5BvolumeControl%5D=true"
+                $scope.selectedProgramEmbedSrc = "//fast.wistia.net/embed/playlists/" + programHashedId + "?media_0_0%5BautoPlay%5D=false&media_0_0%5BcontrolsVisibleOnLoad%5D=false&theme=tab&version=v1&videoOptions%5BautoPlay%5D=true&videoOptions%5BvideoHeight%5D=360&videoOptions%5BvideoWidth%5D=640&videoOptions%5BvolumeControl%5D=true"
             } else {
                 $scope.selectedProgramId = -1;
                 $scope.selectedProgramEmbedSrc = '';
