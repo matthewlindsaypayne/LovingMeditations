a90063885ff2e551a97a624129b3b2e1201c9b65
Matthew Payne <balancetemp@gmail.com>
2016-06-13 17:31:46 -0400

    meditations displaying, meditations formatted, inspirations based on date, corrected scrolling and spy

--- js/loving_meditations_script.js
+++ js/loving_meditations_script.js
@@ -11,15 +11,19 @@
         Parse.serverURL = "https://lmserver-1281.appspot.com/parse";
     });
 
-    lmApp.controller('LovingMeditationsController', ['$scope', function($scope) {
-        
-    }]);
+    lmApp.controller('LovingMeditationsController', function($scope) {
+        $scope.loggedIn = true;
+        $scope.canInvite = true;
+    });
 
     lmApp.controller('InspirationsController', function($scope, $q) {
+        var currentDate = new Date();
+        var originalDate = new Date("May 1, 2016 03:00:00");
+        var dateCount = Math.round(Math.abs((originalDate.getTime() - currentDate.getTime())/(24*60*60*1000)));
         var InspirationsDfd = $q.defer();
         var Inspirations = Parse.Object.extend('Inspirations');
         var queryInspirations = new Parse.Query(Inspirations);
-        queryInspirations.equalTo("author", "Yoda");
+        queryInspirations.equalTo("index", (dateCount % 86));
         queryInspirations.first().then(function (data) {
   	         InspirationsDfd.resolve(data);
         }, function (error) {
@@ -27,10 +31,11 @@
         });
         InspirationsDfd.promise
             .then(function (inspiration) {
-                $scope.currentInspiration = inspiration;
+                $scope.currentInspiration = inspiration.attributes.text + "\n -" + inspiration.attributes.author;
         })
             .catch(function (error) {
-            // do something w/ this
+            // log error
+            console.log("Inspiration retrieval failed.");
         });
     });
     
@@ -82,20 +87,33 @@
     
     lmApp.controller('MeditationsController', function($scope, $http, $sce, $q) {
         $scope.selectedMeditationId = -1;
-        $scope.selectedMeditationEmbedCode = '';
+        $scope.selectedMeditationEmbed = '';
+        $scope.currentPage = 0;
+        $scope.pageSize = 10;
+        
         $http.get("https://api.wistia.com/v1/medias.json?api_password=5450cfdc1299ebebee9129dd19dc06f02db688040667cada25ae12a9924877ee")
             .success(function(data, status, headers, config) {
                 $scope.videosList = data;
                 console.log($scope.videosList);
         });
         
-        $scope.selectMeditation = function(meditationId, meditationEmbedCode) {
+        $scope.numberOfPages = function() {
+            try {
+                return Math.ceil($scope.videosList.length/$scope.pageSize);
+            } catch (err) {
+                console.log("Meditations not yet loaded.");
+            }
+        }
+        
+        $scope.selectMeditation = function(meditationId, meditationHashedId) {
             if ($scope.selectedMeditationId != meditationId) {
                 $scope.selectedMeditationId = meditationId;
-                $scope.selectedMeditationEmbedCode = meditationEmbedCode;
+                $scope.selectedMeditationEmbed = $sce.trustAsHtml("<script charset=\"ISO-8859-1\" src=\"//fast.wistia.com/assets/external/E-v1.js\" async></script><div ng-class=\"wistia_embed wistia_async_" + $sce.trustAsHtml(meditationHashedId) + "\" style=\"height:360px;width:640px\">&nbsp;</div>");
+                console.log("Now Showing!");
             } else {
                 $scope.selectedMeditationId = -1;
-                $scope.selectedMeditationEmbedCode = '';
+                $scope.selectedMeditationEmbed = '';
+                console.log("Now Hiding!");
             }
         };
         
@@ -115,7 +133,13 @@
             var seconds = secondsTotal % 60;
         }
         
-        $scope.trustedEmbedCode = $sce.trustAsHtml($scope.selectedMeditationEmbedCode);
+        $scope.formatMeditationBlurbImage = function(item) {
+            var thumbnailWidth = "250";
+            var thumbnailHeight = "180";
+            return item.replace("image_crop_resized=200x120", "image_crop_resized=" + thumbnailWidth + "x" + thumbnailHeight);
+        }
+        
+        //$scope.trustedEmbedCode = $sce.trustAsHtml($scope.selectedMeditationEmbedCode);
     });
     
     lmApp.filter('ProgramNameFilter', function () {
@@ -131,6 +155,18 @@
         };
     });
     
+    lmApp.filter('startFrom', function() {
+        return function(input, start) {
+            try {
+                start = +start; //parse to int
+                return input.slice(start);
+                }
+            catch(err) {
+                console.log("Meditations not yet loaded.");
+            }
+        }
+    });
+    
     lmApp.controller('LoginController', function($scope, $http, $q) {
         
     });
@@ -143,10 +179,154 @@
         
     });
     
+    lmApp.directive('scrollSpy', function ($window) {
+  return {
+    restrict: 'A',
+    controller: function ($scope) {
+      $scope.spies = [];
+      this.addSpy = function (spyObj) {
+        $scope.spies.push(spyObj);
+      };
+    },
+    link: function (scope, elem, attrs) {
+      var spyElems;
+      spyElems = [];
+
+      scope.$watch('spies', function (spies) {
+        var spy, _i, _len, _results;
+        _results = [];
+
+        for (_i = 0, _len = spies.length; _i < _len; _i++) {
+          spy = spies[_i];
+
+          if (spyElems[spy.id] == null) {
+            _results.push(spyElems[spy.id] = elem.find('#' + spy.id));
+          }
+        }
+        return _results;
+      });
+
+      $($window).scroll(function () {
+        var highlightSpy, pos, spy, _i, _len, _ref;
+        highlightSpy = null;
+        _ref = scope.spies;
+
+        // cycle through `spy` elements to find which to highlight
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          spy = _ref[_i];
+          spy.out();
+
+          // catch case where a `spy` does not have an associated `id` anchor
+          if (spyElems[spy.id].offset() === undefined) {
+            continue;
+          }
+
+          if ((pos = spyElems[spy.id].offset().top) - $window.scrollY <= 0) {
+            // the window has been scrolled past the top of a spy element
+            spy.pos = pos;
+
+            if (highlightSpy == null) {
+              highlightSpy = spy;
+            }
+            if (highlightSpy.pos < spy.pos) {
+              highlightSpy = spy;
+            }
+          }
+        }
+
+        // select the last `spy` if the scrollbar is at the bottom of the page
+        if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+          spy.pos = pos;
+          highlightSpy = spy;
+        }        
+
+        return highlightSpy != null ? highlightSpy["in"]() : void 0;
+      });
+    }
+  };
+});
+
+    lmApp.directive('spy', function ($location, $anchorScroll, anchorSmoothScroll) {
+  return {
+    restrict: "A",
+    require: "^scrollSpy",
+    link: function(scope, elem, attrs, affix) {
+      elem.click(function () {
+        $location.hash(attrs.spy);
+        anchorSmoothScroll.scrollTo(attrs.spy);
+      });
+
+      affix.addSpy({
+        id: attrs.spy,
+        in: function() {
+          elem.addClass('active');
+        },
+        out: function() {
+          elem.removeClass('active');
+        }
+      });
+    }
+  };
+});
+    
+lmApp.service('anchorSmoothScroll', function(){
+    
+    this.scrollTo = function(eID) {
+
+        // This scrolling function 
+        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+        
+        var startY = currentYPosition();
+        var stopY = elmYPosition(eID);
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        var speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (stopY > startY) {
+            for ( var i=startY; i<stopY; i+=step ) {
+                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for ( var i=startY; i>stopY; i-=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+        
+        function currentYPosition() {
+            // Firefox, Chrome, Opera, Safari
+            if (self.pageYOffset) return self.pageYOffset;
+            // Internet Explorer 6 - standards mode
+            if (document.documentElement && document.documentElement.scrollTop)
+                return document.documentElement.scrollTop;
+            // Internet Explorer 6, 7 and 8
+            if (document.body.scrollTop) return document.body.scrollTop;
+            return 0;
+        }
+        
+        function elmYPosition(eID) {
+            var elm = document.getElementById(eID);
+            var y = elm.offsetTop;
+            var node = elm;
+            while (node.offsetParent && node.offsetParent != document.body) {
+                node = node.offsetParent;
+                y += node.offsetTop;
+            } return y;
+        }
+
+    };
+    
+});
+    
 })(window.angular);
 
 
- var lm_menu_height = 0;
+ /*var lm_menu_height = 0;
 jQuery(function($) {
     
     $(document).ready( function() {
@@ -178,4 +358,4 @@ function scrollTo(selectors)
     var selector_top = $(selectors).offset().top - lm_menu_height;
     $('html,body').animate({ scrollTop: selector_top }, 'slow');
 
-} 
+} */
