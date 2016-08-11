e8b8e21b1e9ef606fea1caca82e8cc6f7cd07103
Matthew Payne <balancetemp@gmail.com>
2016-08-11 00:32:21 -0400

    include UserVideo

--- scripts/lmapp.module.js
+++ scripts/lmapp.module.js
@@ -103,7 +103,7 @@
         });
     });
     
-    lmApp.controller('ProgramsController', function($scope, $http, $sce, $timeout, $rootScope, $q) {
+    lmApp.controller('ProgramsController', function($scope, $http, $sce, UserVideo, $timeout, $rootScope, $q) {
         $scope.selectedProgramId = -1;
         $scope.selectedProgramType = 'Patient'; //$scope.programType'';
         $scope.selectedProgramEmbedSrc = '';
